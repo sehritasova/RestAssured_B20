56283995918c2f9797cc1c9cc3396ccadceff886
sehritasova <sehritasova4@gmail.com>
2020-12-14 11:26:53 -0500

    Added Simple Junit 5 Test

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,13 +1,28 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class HelloTest{
 
 
     @Test
-        public void test (){
+    public void test (){
         Assertions.assertEquals(4, 1+3);
-        }
+    }
+
+    @Test
+    public void test2(){
+        // assert 4 time 3 is 12
+        assertEquals(12,4*3);
+
+    }
+
+
+
+
+
+
 
     }
 
