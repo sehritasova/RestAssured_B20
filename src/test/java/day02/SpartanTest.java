170bccbb0047c741e5f1d604736b608510ef1b70
sehritasova <sehritasova4@gmail.com>
2020-12-16 09:37:54 -0500

    Day02 -RestAssured started

---
+++ src/test/java/day02/SpartanTest.java
@@ -0,0 +1,23 @@
+package day02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+
+public class SpartanTest {
+    @DisplayName("Testing /api/spartans endpoint")
+    @Test
+    public void testGetAllSpartan(){
+
+
+    }
+
+
+
+
+
+
+
+}
