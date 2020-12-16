e3eb4b11832b5c6214818da28f2a378fe42e2c4d
sehritasova <sehritasova4@gmail.com>
2020-12-16 12:51:26 -0500

    Added @AfterAll to call the reset method for resetting the value of basePath and baseURI

--- src/test/java/day02/SpartanTest.java
+++ src/test/java/day02/SpartanTest.java
@@ -3,6 +3,7 @@ package day02;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,12 +17,21 @@ public class SpartanTest {
     @BeforeAll
     public static void setUp(){
 
-       RestAssured.baseURI ="http://100.26.101.158:8000";
+        // baseURI and basePath is static fields of RestAssured Class
+        // since we static imported RestAssured, we can access all static field
+        // directly just like it's in our own class here
+        // you can use static way as below
+        RestAssured.baseURI ="http://100.26.101.158:8000";
         RestAssured.basePath= "/api" ;
         // baseURI + basePath + whatever you provided in http method like get post
         // for example :
         // get("/spartans") -- >> get (baseURI + basePath + "/spartans")
 
+    }
+    @AfterAll
+    public static void tearDown(){
+        // resetting the value of baseURI , basePath to original value
+        RestAssured.reset();
     }
 
     @DisplayName("Testing /api/spartans endpoint")
