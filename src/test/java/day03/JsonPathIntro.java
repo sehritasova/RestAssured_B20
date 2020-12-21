25d4f2adceb41f32ddc09554269aaf42da61902a
sehritasova <sehritasova4@gmail.com>
2020-12-21 13:31:37 -0500

    JsonPathIntro Extracting value from Json payload

--- src/test/java/day03/JsonPathIntro.java
+++ src/test/java/day03/JsonPathIntro.java
@@ -26,6 +26,47 @@ public class JsonPathIntro {
     @Test
     public void test1SpartanPayload(){
 
+        // send a request to get 1 spartan
+        // by providing path params with valid id
+        // save it into Response object
+        // NEW: create an object with type JsonPath
+        // by calling the method jsonPath() on response object
+        // extract id, name, gender, phone
+        // and save it into variable of correct type
+
+        Response response = given()
+                                    .pathParam("id",55).
+                            when()
+                                    .get("/spartans/{id}")
+                                    .prettyPeek() ;
+
+
+        //response.prettyPrint();
+
+        // JsonPath is used to navigate through the json payload
+        // and extract the value according to the valid " jsonpath " provided
+        JsonPath jp = response.jsonPath();
+        int myId = jp.getInt("id");
+        String myName = jp.getString("name");
+        String myGender = jp.getString("gender");
+        long myPhone = jp.getLong("phone");
+
+        System.out.println("myId ="+ myId);
+        System.out.println("myName = " + myName);
+        System.out.println("myGender = " + myGender);
+        System.out.println("myPhone = " + myPhone);
+
+
     }
+
+
+
+
+
+
+
+
+
+
 
 }
