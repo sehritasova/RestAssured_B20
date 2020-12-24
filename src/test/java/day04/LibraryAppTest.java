0c072d1690560a5e2a35d8c1db8ec79e7baf49ac
sehritasova <sehritasova4@gmail.com>
2020-12-24 14:08:02 -0500

    Day04 - LibraryAppTest Starter

---
+++ src/test/java/day04/LibraryAppTest.java
@@ -0,0 +1,19 @@
+package day04;
+import org.junit.jupiter.api.*;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+
+public class LibraryAppTest {
+
+    @BeforeAll
+    public static void setUp(){
+        baseURI = "http://library1.cybertekschool.com";
+        basePath = "/rest/v1" ;
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        reset();
+    }
+}
