b3819fa2f4ebd36881fcf6d9216ed78f9c5a020e
sehritasova <sehritasova4@gmail.com>
2020-12-27 12:49:24 -0500

    Day04 - LibraryAppTest /login Test extracting token after validation in the chain

--- src/test/java/day04/LibraryAppTest.java
+++ src/test/java/day04/LibraryAppTest.java
@@ -25,5 +25,30 @@ public class LibraryAppTest {
         Librarian1  password		KNPXrm3S
          */
 
+       String myToken =
+                given()
+                        .log().all()
+                        .contentType( ContentType.URLENC  )
+                        .formParam("email", "librarian69@library")
+                        .formParam("password","KNPXrm3S").
+                when()
+                        .post("/login").
+                then()
+                        .log().all()
+                        .assertThat()
+                        .statusCode( is(200))
+                        .contentType(ContentType.JSON)
+                        .body("token", is( not( emptyString() ) )  )
+                    .extract()
+                        .jsonPath()
+                        .getString("token")
+        ;
+        System.out.println("myToken = \n" + myToken);
+        // How to extract some data out of response object
+        // after doing validation in then section
+        // without breaking the chain -->> use extract() method that return
     }
+
+
+
 }
